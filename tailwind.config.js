2e30f39ce90060ca72f351dcd6465f0ed5c8dc03
Taladeogo <ataladeogo@gmail.com>
2022-08-24 16:50:06 +0100

    Practice GSAP Animation

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  content: [
+    "./build/**/*.{html,js}"
+  ],
+  theme: {
+    extend: {
+      animation: {
+        'spin-slow': 'spin 10s linear infinite',
+      }
+    },
+  },
+  plugins: [],
+}
